199b6a93601a9faa412d055b4d3de04a57c93fba
Rohan Gupta <7039426154rg@gmail.com>
2024-04-07 22:38:15 +0530

    renamed from js to jsx

---
+++ src/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Calculator from './components/calcComponents/Calc'
+import SimonGame from './components/simonSays/simon'
+
+function Main() {
+  return (
+  <>
+  <Calculator />
+  <SimonGame />
+  </>
+  )
+}
+
+export default Main
