77a62bb559e13fce0f1166aba309a8346449f3ca
Rohan Gupta <7039426154rg@gmail.com>
2024-04-11 23:12:56 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Import BrowserRouter as Router
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './Main';
-import Calculator from './pages/calcComponents/Calc'; // Import Calculator component
-import SimonGame from './pages/simonSays/SimonGame'; // Import SimonGame component
+import Calculator from './pages/calcComponents/Calc';
+import SimonGame from './pages/simonSays/SimonGame'; 
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-      <Route path="/" exact element={<Main />} /> {/* Set Main as the default component */}
-      <Route path="/calculator" element={<Calculator />} /> {/* Route for Calculator */}
-      <Route path="/simon" component={<SimonGame />} /> {/* Route for SimonGame */}
+      <Route path="/" element={<Main />} />
+      <Route path="/calculator" element={<Calculator />} /> 
+      <Route path="/simon" component={<SimonGame />} />
       </Routes>
     </BrowserRouter>
     </div>
