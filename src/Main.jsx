d880c336b20a9ec337c689dead1ed3ec64b29ed3
Rohan Gupta <7039426154rg@gmail.com>
2024-04-12 23:28:25 +0530

    started work for portfolio

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Leftbar from './pages/LeftBar/Leftbar'
 
 function Main() {
   return (
-    <div className="container mx-auto p-4">
+    <div>
+      <div className="bg-gradient-to-br from-sky-500 to-white flex items-center justify-center h-screen px-24">
+      <div className="w-5/6 h-5/6 flex items-center justify-center">
+        <Leftbar />
+      </div>
+    </div>
       <div className="flex justify-center space-x-4 mb-4">
         <Link to="/calculator" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Calculator</Link>
         <Link to="/simon" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Simon Game</Link>
