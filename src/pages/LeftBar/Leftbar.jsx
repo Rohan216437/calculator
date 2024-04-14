ca7cc667ae49d63f617558bb09826a8ef17e0098
Rohan Gupta <7039426154rg@gmail.com>
2024-04-14 22:32:39 +0530

    LeftBar , changes

--- src/pages/LeftBar/Leftbar.jsx
+++ src/pages/LeftBar/Leftbar.jsx
@@ -1,4 +1,5 @@
 import { GitHub, Instagram, LinkedIn, Twitter } from '@mui/icons-material'
+import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
 // import { HashLink as Link } from 'react-router-hash-link'
 import { Link } from 'react-router-dom';
 import { useState } from 'react'
@@ -11,69 +12,68 @@ const Leftbar = () => {
   }
 
   return (
-    <div className="flex-1 text-white overflow-hidden">
-     <div className="md:flex md:flex-col md:justify-center md:h-45vh md:pt-8 lg:flex lg:flex-col lg:justify-center lg:h-24vh lg:pt-8 lg:mb-8">
-     <div className='top mb-24 md:mb-8 lg:mb-8'>
-  <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">Rohan Gupta</h1>
-  <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold my-4 md:my-6 lg:my-6">Web developer</h3>
-  <p className="text-base md:text-lg lg:text-xl text-blue-300">I built accessible, inclusive <br /> products and digital experiences for the web</p>
-</div>
-
-      <div className='center'>
-        <Link
-          smooth
-          to='#about'
-          onClick={() => handleLinkClick('about')}
-          className={`${activeLink === 'about' ? 'active-link' : ''}`}
-        >
-          ABOUT
-        </Link>
-        <Link
-          smooth
-          to='#skills'
-          onClick={() => handleLinkClick('skills')}
-          className={`${activeLink === 'skills' ? 'active-link' : ''}`}
-        >
-          SKILLS
-        </Link>
-        <Link
-          smooth
-          to='#projects'
-          onClick={() => handleLinkClick('projects')}
-          className={`${activeLink === 'projects' ? 'active-link' : ''}`}
-        >
-          PROJECTS
-        </Link>
-      </div>
-      <div className='bottom'>
-        <ul>
-          <li>
-            <a href='#' target='_blank'>
-              <GitHub className='icon' />
-            </a>
-          </li>
-          <li>
-            <a href='#'>
-              <Instagram className='icon' />
-            </a>
-          </li>
-          <li>
-            <a href='#'>
-              <Twitter className='icon' />
-            </a>
-          </li>
-          <li>
-            <a
-              href='#'
-              target='_blank'
+      <div className="text-white overflow-hidden m-4">
+        <div className="md:flex md:flex-col md:justify-center md:h-45vh md:pt-8 lg:flex lg:flex-col lg:justify-center lg:h-24vh lg:pt-8 lg:mb-8">
+          <div className='top mb-24 md:mb-8 lg:mb-8'>
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">Rohan Gupta</h1>
+            <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold my-4 md:my-6 lg:my-6">Web developer</h3>
+            <p className="text-base md:text-lg lg:text-xl text-blue-300">I am an enthusiastic coder, <br />dedicated to mastering FullStack Web Development.</p>  
+          </div>
+          <div className='center mb-20 md:mb-10 hidden md:flex flex-col '>
+            <Link
+              smooth
+              // to='about'
+              onClick={() => handleLinkClick('about')}
+              className={`text-blue-300 hover:text-white font-semibold flex items-center ${activeLink === 'about' ? 'text-white' : ''}`}
             >
-              <LinkedIn className='icon' />
-            </a>
-          </li>
-        </ul>
+              ABOUT {activeLink === 'about' ? <span className='pl-5 w-20'><TrendingFlatIcon /></span> : ''}
+            </Link>
+            <Link
+              smooth
+              // to='skills'
+              onClick={() => handleLinkClick('skills')}
+              className={`text-blue-300 hover:text-white font-semibold flex items-center ${activeLink === 'skills' ? 'text-white' : ''}`}
+            >
+              SKILLS {activeLink === 'skills' ? <span className='pl-5 w-20'><TrendingFlatIcon /></span> : ''}
+            </Link>
+            <Link
+              smooth
+              // to='projects'
+              onClick={() => handleLinkClick('projects')}
+              className={`text-blue-300 hover:text-white font-semibold flex items-center ${activeLink === 'projects' ? 'text-white' : ''}`}
+            >
+              PROJECTS {activeLink === 'projects' ? <span className='pl-5 w-20'><TrendingFlatIcon /></span> : ''}
+            </Link>
+          </div>
+          <div>
+            <ul className='list-none flex gap-8'>
+              <li>
+                <a href='#' target='_blank'>
+                  <GitHub className='icon text-blue-300 hover:text-white transition-colors duration-200' />
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <Instagram className='icon text-blue-300 hover:text-white transition-colors duration-200' />
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <Twitter className='icon text-blue-300 hover:text-white transition-colors duration-200' />
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  target='_blank'
+                >
+                  <LinkedIn className='icon text-blue-300 hover:text-white transition-colors duration-200' />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-     </div>
-    </div>
   )
 }
 
