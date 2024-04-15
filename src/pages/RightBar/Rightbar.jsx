995514009d41278b336daf5cd2299e0e2c7a5a92
Rohan Gupta <7039426154rg@gmail.com>
2024-04-15 23:01:39 +0530

    minor change - about tag added

--- src/pages/RightBar/Rightbar.jsx
+++ src/pages/RightBar/Rightbar.jsx
@@ -1,9 +1,9 @@
-
+import About from "./About"
 
 const Rightbar = () => {
   return (
-    <div className='rightbar'>
-      Right Bar here
+    <div className="flex-1 text-white/75 overflow-y-scroll overflow-x-hidden h-90vh scrollbar-none md:h-47vh md:mb-8 lg:h-70vh lg:mb-8">
+      <About />
     </div>
   )
 }
