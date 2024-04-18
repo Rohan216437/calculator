ded363f88ed0da05b3fe57741bf2ad5ef9961105
Rohan Gupta <7039426154rg@gmail.com>
2024-04-18 21:06:04 +0530

    skills object file

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const skills = [
+    {
+      id: '1',
+      name: 'HTML5',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fhtml-5--v1.png&w=256&q=75'
+    },
+    {
+      id: '2',
+      name: 'CSS3',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fcss3.png&w=256&q=75'
+    },
+    {
+      id: '3',
+      name: 'Javascript',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fjavascript--v1.png&w=256&q=75'
+    },
+    {
+      id: '4',
+      name: 'Sass',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fsass.png&w=256&q=75'
+    },
+    {
+      id: '5',
+      name: 'ReactJS',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fultraviolet%2F120%2Fnull%2Freact--v1.png&w=256&q=75'
+    },
+    {
+      id: '6',
+      name: 'redux',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fredux.png&w=256&q=75'
+    },
+    {
+      id: '7',
+      name: 'Bootstrap',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fbootstrap.png&w=256&q=75'
+    },
+    {
+      id: '8',
+      name: 'NodeJS',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Ffluency%2F144%2Fnull%2Fnode-js.png&w=256&q=75'
+    },
+    {
+      id: '9',
+      name: 'ExpressJs',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fios%2F150%2Fnull%2Fexpress-js.png&w=256&q=75'
+    },
+    {
+      id: '10',
+      name: 'MongoDB',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fexternal-tal-revivo-color-tal-revivo%2F96%2Fnull%2Fexternal-mongodb-a-cross-platform-document-oriented-database-program-logo-color-tal-revivo.png&w=256&q=75'
+    },
+    {
+      id: '11',
+      name: 'Java',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fjava-coffee-cup-logo--v1.png&w=256&q=75'
+    },
+    {
+      id: '12',
+      name: 'Native',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Freact-native.png&w=256&q=75'
+    },
+    {
+      id: '13',
+      name: 'Git',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fgit.png&w=256&q=75'
+    },
+    {
+      id: '14',
+      name: 'Github',
+      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fgit.png&w=256&q=75'
+    }
+  ]
