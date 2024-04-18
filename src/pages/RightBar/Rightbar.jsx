ad2bda41652e9f758775ddcea75e2b20f4cf56b3
Rohan Gupta <7039426154rg@gmail.com>
2024-04-18 21:06:39 +0530

    rightbar changes

--- src/pages/RightBar/Rightbar.jsx
+++ src/pages/RightBar/Rightbar.jsx
@@ -1,9 +1,12 @@
 import About from "./About"
+import Skills from "./Skills"
 
-const Rightbar = () => {
+const Rightbar = ({activeSection}) => {
   return (
     <div className="flex-1 text-white/75 overflow-y-scroll overflow-x-hidden h-90vh scrollbar-none md:h-47vh md:mb-8 lg:h-70vh lg:mb-8">
-      <About />
+      {activeSection === 'about' && <About />}
+      {activeSection === 'projects' && <About />}
+      {activeSection === 'skills' && <Skills />}
     </div>
   )
 }
