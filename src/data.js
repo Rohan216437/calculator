5be025303a6d0b1a6a22ad81e525616828ef7936
Rohan Gupta <7039426154rg@gmail.com>
2024-04-23 23:13:52 +0530

    data file changes

--- src/data.js
+++ src/data.js
@@ -1,72 +1,118 @@
+// ICONS link is from this website --> https://devicon.dev/
+
 export const skills = [
     {
       id: '1',
       name: 'HTML5',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fhtml-5--v1.png&w=256&q=75'
+      icon : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      // icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fhtml-5--v1.png&w=256&q=75'
     },
     {
       id: '2',
       name: 'CSS3',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fcss3.png&w=256&q=75'
+      icon : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+      // icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fcss3.png&w=256&q=75'
     },
     {
       id: '3',
       name: 'Javascript',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fjavascript--v1.png&w=256&q=75'
+      icon : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      // icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fjavascript--v1.png&w=256&q=75'
     },
     {
       id: '4',
-      name: 'Sass',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fsass.png&w=256&q=75'
-    },
-    {
-      id: '5',
       name: 'ReactJS',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fultraviolet%2F120%2Fnull%2Freact--v1.png&w=256&q=75'
+      icon : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      // icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fultraviolet%2F120%2Fnull%2Freact--v1.png&w=256&q=75'
     },
+    // {
+    //   id: '5',
+    //   name: 'redux',
+    //   icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fredux.png&w=256&q=75'
+    // },
     {
       id: '6',
-      name: 'redux',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fredux.png&w=256&q=75'
+      name: 'Bootstrap',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg'
+      // icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fbootstrap.png&w=256&q=75'
     },
     {
       id: '7',
-      name: 'Bootstrap',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fbootstrap.png&w=256&q=75'
+      name: 'NodeJS',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg'
+      // icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Ffluency%2F144%2Fnull%2Fnode-js.png&w=256&q=75'
     },
     {
       id: '8',
-      name: 'NodeJS',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Ffluency%2F144%2Fnull%2Fnode-js.png&w=256&q=75'
+      name: 'C#',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg'
     },
     {
       id: '9',
-      name: 'ExpressJs',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fios%2F150%2Fnull%2Fexpress-js.png&w=256&q=75'
-    },
+      name: 'ASP.NET',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-plain.svg'
+    },    
     {
       id: '10',
-      name: 'MongoDB',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fexternal-tal-revivo-color-tal-revivo%2F96%2Fnull%2Fexternal-mongodb-a-cross-platform-document-oriented-database-program-logo-color-tal-revivo.png&w=256&q=75'
+      name: 'Python',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg'
     },
     {
       id: '11',
-      name: 'Java',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fjava-coffee-cup-logo--v1.png&w=256&q=75'
+      name: 'Git',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg'
+      // icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fgit.png&w=256&q=75'
     },
     {
       id: '12',
-      name: 'Native',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Freact-native.png&w=256&q=75'
-    },
-    {
-      id: '13',
-      name: 'Git',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fgit.png&w=256&q=75'
-    },
-    {
-      id: '14',
       name: 'Github',
-      icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fgit.png&w=256&q=75'
+      icon : 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg'
+      // icon: 'https://jigarsable.vercel.app/_next/image?url=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F144%2Fnull%2Fgit.png&w=256&q=75'
     }
   ]
+
+export const apk = [
+  {
+    id : "1",
+    name : "Calculator",
+    parameter : "/calculator",
+    icon : "https://img.icons8.com/3d-fluency/94/calculator.png"
+  },
+  {
+    id : "2",
+    name : "Simon Game",
+    parameter : "/simon",
+    icon : "https://img.icons8.com/3d-fluency/94/controller.png"
+  },
+  {
+    id : "3",
+    name : "To-DO List",
+    parameter : "/",
+    icon : "https://img.icons8.com/3d-fluency/94/test-passed.png"
+  },
+  {
+    id : "4",
+    name : "Stock Graph",
+    parameter : "/",
+    icon : "https://img.icons8.com/3d-fluency/94/combo-chart.png"
+  },
+  {
+    id : "5",
+    name : "Amazon Clone",
+    parameter : "/",
+    icon : "https://img.icons8.com/3d-fluency/94/amazon.png"
+  },
+  {
+    id : "6",
+    name : "DashBoard Page",
+    parameter : "/",
+    icon : "https://img.icons8.com/3d-fluency/94/control-panel.png"
+  },
+  // {
+  //   id : "7",
+  //   name : "Calculator",
+  //   parameter : "/",
+  //   icon : "https://img.icons8.com/3d-fluency/94/calculator.png"
+  // },
+
+]
